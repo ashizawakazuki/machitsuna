cdbe40010e53567d231136a1b70da5c4286f5c39
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-07-06 09:38:06 +0900

    feat:トップページの作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import TalentBank from './pages/TalentBank';
+import Events from './pages/Events';
+import Titles from './pages/Titles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/talent-bank" element={<TalentBank />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/titles" element={<Titles />} />
+        {/* 今後ここに他の画面も追加予定 */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>地域コミュニティイベント管理アプリ</h1>
+      <p className="description">
+        地域のイベント情報やスキルを持つ人を見つけ、参加・交流して称号を獲得しましょう。
+      </p>
+
+      <div className="card-container">
+        <Link to="/talent-bank" className="card">
+          <img src="/images/people.png" alt="人材バンク" />
+          <span>人材バンク</span>
+        </Link>
+        <Link to="/events" className="card">
+          <img src="/images/calendar.png" alt="イベント一覧" />
+          <span>イベント一覧</span>
+        </Link>
+        <Link to="/titles" className="card">
+          <img src="/images/medal.png" alt="称号" />
+          <span>称号</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
