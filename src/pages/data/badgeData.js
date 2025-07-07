6a6f8aca21062ca59ffd1627d3c6f6f6d5ebb879
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-07-07 14:50:28 +0900

    fix:称号画面の各称号分離

---
+++ src/pages/data/badgeData.js
@@ -0,0 +1,9 @@
+const badgeData = [
+  { name: '地域デビュー', count: 1, icon: '🔰' },
+  { name: 'つながりビギナー', count: 3, icon: '🌱' },
+  { name: 'まちづくりプレイヤー', count: 5, icon: '⭐' },
+  { name: 'まちのつなぎ手', count: 10, icon: '🏅' },
+  { name: 'まちつなマスター', count: 20, icon: '👑' },
+];
+
+export default badgeData;
