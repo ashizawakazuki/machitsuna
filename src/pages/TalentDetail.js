81f72383c6c240c8e73070b51ba30de88c5fb01e
ashizawakazuki <ashizawa.kazuki@gmail.com>
2025-07-07 11:08:43 +0900

    fix:詳細画面の修正

--- src/pages/TalentDetail.js
+++ src/pages/TalentDetail.js
@@ -27,18 +27,21 @@ const talentData = [
   
     return (
       <div className="talent-detail">
+        <Link to="/talent-bank" className="back-link">{'＜'}一覧へ戻る</Link>
         <h2>詳細画面</h2>
+        <div className="event-content">
+          <p><strong>名前：</strong>{talent.name} さん</p>
+          <p><strong>特技・スキル：</strong>{talent.skill}</p>
+          <p><strong>カテゴリー：</strong>{talent.category}</p>
+          <p><strong>指導料：</strong>無料</p>
+          <p><strong>可能な時：</strong>土日 夕方</p>
+          <p><strong>資格：</strong>〇〇</p>
+          <p><strong>現在の活動：</strong>〇〇〇〇</p>
+          <p><strong>可能地域：</strong>〇〇〇</p>
+          <p><strong>一言：</strong>皆様とお会いできるのを楽しみにしております</p>
+        </div>
         <button className="contact-button">管理者を通して依頼</button>
-        <p><strong>名前：</strong>{talent.name} さん</p>
-        <p><strong>特技・スキル：</strong>{talent.skill}</p>
-        <p><strong>カテゴリー：</strong>{talent.category}</p>
-        <p><strong>指導料：</strong>無料</p>
-        <p><strong>可能な時：</strong>土日 夕方</p>
-        <p><strong>資格：</strong>〇〇</p>
-        <p><strong>現在の活動：</strong>〇〇〇〇</p>
-        <p><strong>可能地域：</strong>〇〇〇</p>
-        <p><strong>一言：</strong>皆様とお会いできるのを楽しみにしております</p>
-        <Link to="/talent-bank" className="back-link">一覧へ戻る</Link>
+
       </div>
     );
   };
